dbb7d050558e32d164c7ef52d4b29e7575ee0e81
Edson Garcia <ednosmab@yahoo.com.br>
2024-08-28 11:10:21 -0300

    componente incluido

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
+import DisplaySorteio from "@/components/DisplaySorteio";
+
 export default function Home() {
   return (
     <div className="container">
       <h1>Mega Sena</h1>
+        <DisplaySorteio/>
     </div>
   );
 }
